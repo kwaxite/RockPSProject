a4b529af1d52f360dbbb4278a705c8915f5c0cc0
kwaxite <146014461+kwaxite@users.noreply.github.com>
2023-12-09 14:09:41 +0000

    create buttons

--- rockps.js
+++ rockps.js
@@ -40,40 +40,57 @@ function playRound (playerSelection, computerSelection){
 // x = x.split(' ')
 // console.log(x[1])
 
-function game(){
-    let result = '';
-    playerCount = 0;
-    computerCount = 0;
-    alert(`5 rounds of Rock Paper Scissors against the computer`)
-    for (let i = 1; i < 6; i++){
-        alert(`Round ${i}`)
-        playerSelection = prompt("Choose one: rock or paper or scissors")
-        let computerSelection = getComputerChoice()
-        result = playRound (playerSelection, computerSelection)
-        console.log(result)
-        if ( result.split(' ')[1] === 'Win!'){
-            playerCount = playerCount + 1
-            alert(`Yay! you win round ${i}`)
-        } else if ( result.split(' ')[1] === 'Lose!'){
-            alert(`The computer wins round ${i}`)
-            computerCount = computerCount + 1
-        } else { alert ('Its a draw!')}
-    }
-    console.log (`computer wins = ${computerCount}`)
-    console.log (`player wins = ${playerCount}`)
-    if (playerCount > computerCount){
-        console.log("You win the game")
-        document.body.innerHTML = "<h1>You win the game</h1>'"
-    } else if (playerCount < computerCount){
-        console.log("Computer wins the game")
-        document.body.innerHTML = "<h1>Computer wins the game</h1>'"
-    } else {
-        console.log("It's a draw")
-        document.body.innerHTML = '<h1> It\'s a draw!</h1>';
-    }
+// function game(){
+//     let result = '';
+//     playerCount = 0;
+//     computerCount = 0;
+//     alert(`5 rounds of Rock Paper Scissors against the computer`)
+//     for (let i = 1; i < 6; i++){
+//         alert(`Round ${i}`)
+//         playerSelection = prompt("Choose one: rock or paper or scissors")
+//         let computerSelection = getComputerChoice()
+//         result = playRound (playerSelection, computerSelection)
+//         console.log(result)
+//         if ( result.split(' ')[1] === 'Win!'){
+//             playerCount = playerCount + 1
+//             alert(`Yay! you win round ${i}`)
+//         } else if ( result.split(' ')[1] === 'Lose!'){
+//             alert(`The computer wins round ${i}`)
+//             computerCount = computerCount + 1
+//         } else { alert ('Its a draw!')}
+//     }
+//     console.log (`computer wins = ${computerCount}`)
+//     console.log (`player wins = ${playerCount}`)
+//     if (playerCount > computerCount){
+//         console.log("You win the game")
+//         document.body.innerHTML = "<h1>You win the game</h1>'"
+//     } else if (playerCount < computerCount){
+//         console.log("Computer wins the game")
+//         document.body.innerHTML = "<h1>Computer wins the game</h1>'"
+//     } else {
+//         console.log("It's a draw")
+//         document.body.innerHTML = '<h1> It\'s a draw!</h1>';
+//     }
+// }
+
+// game()
+
+
+
+const buttons = document.querySelectorAll('button')
+
+console.log(buttons)
+
+const rock = buttons[0]
+rock.textContent = 'ROCK'
+rock.style.cssText = "font-size: 2rem; background-color:yellow"
+const paper = buttons[1]
+paper.textContent = 'PAPER'
+paper.style.cssText = "font-size: 2rem; background-color:pink"
+const scissor = buttons[2]
+scissor.textContent = 'SCISSORS'
+scissor.style.cssText = "font-size: 2rem; background-color:lightgreen"
+
+const select = () => {  
 }
-
-game()
-
-
 
